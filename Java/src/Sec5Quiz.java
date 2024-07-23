ce1ef64311ed9d4055f7e1e489749a921078687f
Keshav_Pai <keshavpai927@gmail.com>
2024-07-23 20:26:40 +0530

    After One Drive Messup

--- Java/src/Sec5Quiz.java
+++
@@ -1,7 +0,0 @@
-public class Sec5Quiz {
-    public static void main(String[] args){
-    int a = 10;
-    System.out.println(a++);
-    a++;
-}
-}
--- Java/src/unicode.java
+++
@@ -1,9 +0,0 @@
-public class unicode {
-    public static void main(String[] args) {
-        char x = 0xA8FE;
-        for (char c = 0xA8F2;c<=0xA8FE; c++) {
-            System.out.println(c);
-        }
-//        System.out.println(x);
-    }
-}
